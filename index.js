6e93f540109c13859634de40d39e1663c18e69e2
Rafael Melo <rsmelo_@hotmail.com>
2019-06-11 03:00:25 -0300

    Testing fallback with fuzzy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const highlightTerm = (text, wordToBeHighlighted) => {
-  const trimmedText = text
+  let trimmedText = text
     .replace(/(<([^>]+)>)/gi, item =>
       item
         .split("")
@@ -8,7 +8,7 @@ const highlightTerm = (text, wordToBeHighlighted) => {
     )
     .replace(/[^A-Z0-9]/gi, "_")
     .toLowerCase();
-  const regex = wordToBeHighlighted
+  let regex = wordToBeHighlighted
     .replace(/\s/gi, "")
     .split("")
     .map((letter, index, array) => {
@@ -34,6 +34,35 @@ const highlightTerm = (text, wordToBeHighlighted) => {
     });
     return item;
   });
+  if (!termsLength.length > 0) {
+    const stringSimilarity = require("string-similarity");
+    let similarity,
+      higherSimilarity = 0.00000001,
+      finalIndex,
+      crazey = [];
+    trimmedText.split("").reduce(function(a, b, index) {
+      similarity = stringSimilarity.compareTwoStrings(
+        wordToBeHighlighted.normalize("NFD").replace(/[\u0300-\u036f]/gi, ""),
+        a
+      );
+      if (similarity >= higherSimilarity) {
+        higherSimilarity = similarity;
+        crazey.unshift(a);
+      }
+      if (a.length === wordToBeHighlighted.length) {
+        a = a.substr(1);
+      }
+      return a + b;
+    });
+    trimmedText = trimmedText.replace(crazey[1], item => {
+      const regexExec = trimmedText.match(item);
+      termsLength.push({
+        initPosition: regexExec.index,
+        finalPosition: regexExec.index + item.length
+      });
+      return item;
+    });
+  }
   // Get the actual terms that will be highlighted
   const finalTerms = termsLength.map(objectTerm => {
     const { initPosition, finalPosition } = objectTerm;
@@ -48,8 +77,31 @@ const highlightTerm = (text, wordToBeHighlighted) => {
     );
     text = text.replace(finalPattern, "<mark>$&</mark>");
   });
+
+  console.log("=====> final", text);
+
   return text;
 };
+
+highlightTerm(
+  `10/06/2019
+Terceira Vice-presidência
+
+Cartório da 12ª Câmara Cível - Unidade Afonso
+
+Convocação de Câmara
+
+DÉCIMA SEGUNDA CÂMARA CÍVEL - Ordem do dia para julgamento - Sessão de Julgamento a realizar-se no dia 19/06/2019, QUARTA-FEIRA, às 13h30min, no plenário VIII da Unidade Afonso Pena. Cartório da Décima Segunda Câmara Cível. Escrivã: Grazziane Vargas Leonel de Carvalho.
+
+Apelação Cível
+
+
+02437 - 5005789-20.2016.8.13.0672 @ (Processo Eletrônico) Sete Lagoas;
+
+Apelante(s) - GUSTAVO GONCALVES TORRES DA CRUZ; Apelado(a)(s) - GOOGLE BRASIL INTERNET LTDA; GOSHME SOLUCOES PARA INTERNET LTDA - JUSBRASIL; Relator - Des(a). José Flávio de Almeida; Autos incluídos na pauta de julgamento de 19/06/2019, às 13:30 horas Adv -FABIO RIVELLI, MÁRIO PINTO RODRIGUES DA COSTA FILHO, THIAGO ROCHESTER AVILA.`,
+  "GOOGLE BRASIL INTERNET LTDA"
+  // "Goshme Soluções para internet ltda me"
+);
 
 module.exports = {
   highlightTerm
